e3af094bbf4d50b0522540bac33020ba14c8fa4a
VieiraHeitor <heitormaveiro@yahoo.com.br>
2021-06-13 14:28:41 -0300

    blockchain tests added

--- test/blockchain_test.js
+++ test/blockchain_test.js
@@ -2,43 +2,57 @@ const mocha = require('mocha')
 const chai = require('chai')
 const block = require('../src/block')
 const blockchain = require('../src/blockchain')
+const { expect } = require('chai')
 
 describe("Testing Blockchain modules", function(){
     
     test_blockchain = new blockchain()
+    test_blockchain.addBlock(new block("02/25/2021", { amount: 0 }))
 
-    describe("Block creation", function(){
+    describe("Blockchain creation", function(){
         it("Should be of type object", function(){
-            chai.expect(test_block).to.be.a('Object')
+            chai.expect(test_blockchain).to.be.a('Object')
         })
 
-        it("Should populate data", function(){
-            chai.expect(test_block.data.amout).to.be.equal(40)
+        it("Should have a chain object", function(){
+            chai.expect(test_blockchain).to.haveOwnProperty('chain')
         })
-    })
 
-    describe("Hash creation", function(){
-        it("Hash should not be of type null", function(){
-            chai.expect(test_block.hash).to.not.be.a('null')
+        it("Should have a difficulty attribute", function(){
+            chai.expect(test_blockchain).to.haveOwnProperty('difficulty')
         })
-    })
 
-    describe("Block mining", function(){
-        it("Mined block should match block hash",function(){
-            chai.expect(mined_block_2).to.have.string(test_block.hash)
+        it("Should populate chain object with blocks", function(){
+            chai.expect(test_block.chain).to.be.not.a('null')
         })
-    })
-
-    describe("Mining difficulty shoud be variable", function(){
+    
+        it("Should create genesis block (index = 0)", function(){
+            chai.expect(test_blockchain.chain[0].index).to.be.equal(0)
+        })
         
-        it("Start with one 0 when difficulty equal 1",function(){
-            chai.expect(mined_block_1).to.be.match(/^[0]/g)
-        })
-
-        it("Start with two 0s when difficulty equal 2",function(){
-            chai.expect(mined_block_2).to.be.match(/^[0]{2}/g)
+        it("Should create genesis block (previousHash = 0)", function(){
+            chai.expect(test_blockchain.chain[0].previousHash).to.be.equal('0')
         })
     })
 
+    describe("Blockchain validation",function(){
+        it("Should validate the chain",function(){
+            chai.expect(test_blockchain.chainValidation()).to.be.true
+        })
+    })
 
+ 
+    describe("Blockchain difficulty",function(){
+        test_blockchain.addBlock(new block("02/26/2021", { amount: 42 }))
+        
+        it("Should have standard difficulty equals to chain size",function(){
+            chai.expect(test_blockchain.difficulty).to.be.equal(test_blockchain.chain.length-1)
+        })
+    
+        it("Should have custom difficulty equals to value passed",function(){
+            alt_difficulty = 1
+            test_blockchain.addBlock(new block("02/27/2021", { amount: 42 }),alt_difficulty)
+            chai.expect(test_blockchain.difficulty).to.be.equal(alt_difficulty)
+        })
+    })
 })
